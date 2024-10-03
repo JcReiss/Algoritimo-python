d46ef0ebcb842edfe84c3898bd6ba087b0908594
JcReiss <cesinhajunior17@hotmail.com>
2024-10-02 21:15:16 -0400

    prova

---
+++ prova.py
@@ -0,0 +1,8 @@
+
+#? List comprhension questão 01
+
+#* questão 5
+
+nomes={"Ana","Bruno", "Carlos", "Alice", "Emanuel","Augusto", "Rafael", "Albert"}
+    Inicial= list(filter(nomes.startswith"A"))
+print(inicial)
